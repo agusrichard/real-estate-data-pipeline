8da1514d1d39b8e26a415e84e32db59a796493d9
Agus Richard <agus.richard21@gmail.com>
2026-03-01 19:11:34 +0700

    feat(terraform): add modules for lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,16 @@ module "iam" {
   bucket_arn = module.s3.bucket_arn
   role_name  = "real-estate-lambda-role"
 }
+
+module "ingest_kaggle_lambda" {
+  source        = "./modules/lambda"
+  function_name = "ingest-kaggle"
+  role_arn      = module.iam.lambda_role_arn
+  zip_path      = "../lambdas/ingest_kaggle/lambda.zip"
+  timeout       = 300
+  memory_size   = 512
+  environment_variables = {
+    BUCKET_NAME = module.s3.bucket_name
+    SOURCE_KEY  = "raw/kaggle/source/realtor-data.csv"
+  }
+}
